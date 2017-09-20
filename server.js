fb94f6d33760e2298666a21d33d57eed6ce646d4
Abhishek Chowdhury <Portal9001@Abhisheks-MBP.hsd1.ca.comcast.net>
2017-09-20 00:11:14 -0700

    Heroku config

--- server.js
+++ server.js
@@ -11,6 +11,6 @@ app.get('*', function(req, res) {
 	res.sendFile(path.resolve(__dirname, 'public', 'index.html'))
 });
 
-app.listen(3000, function(){
-	console.log('web-server');
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
